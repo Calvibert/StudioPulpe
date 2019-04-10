f1e48d0e8e64e453a7af79b85e1a841747da843a
Samuel Dufresne <samuel.dufresne@live.com>
2019-04-09 21:02:11 -0400

    working 404

--- 404.php
+++ 404.php
@@ -6,20 +6,20 @@
   <title>Not Found</title>
   <meta content="Not Found" property="og:title">
   <meta content="width=device-width, initial-scale=1" name="viewport">
-  <link href="css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="css/webflow.css" rel="stylesheet" type="text/css">
-  <link href="css/studiopulpev3.webflow.css" rel="stylesheet" type="text/css">
+  <link href="/css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="/css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="/css/studiopulpev3.webflow.css" rel="stylesheet" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>
   <script type="text/javascript">WebFont.load({  google: {    families: ["Droid Sans:400,700","PT Sans:400,400italic,700,700italic"]  }});</script>
   <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
   <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
-  <link href="images/pulpe_logo_v05.png" rel="shortcut icon" type="image/x-icon">
-  <link href="images/webclip.png" rel="apple-touch-icon">
+  <link href="/images/pulpe_logo_v05.png" rel="shortcut icon" type="image/x-icon">
+  <link href="/images/webclip.png" rel="apple-touch-icon">
 </head>
 <body class="body">
   <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar thanks w-nav">
     <a href="index.html" class="brand merci w-nav-brand">
-      <div class="letters-container"><img src="images/p_1_Default.png" alt="" class="letter-p1"><img src="images/u_Default.png" alt="" class="letter-u"><img src="images/l_Default.png" alt="" class="letter-l"><img src="images/p_2_Default.png" alt="" class="letter-p2"><img src="images/e_Default.png" alt="" class="letter-e"><img src="images/_Default.png" alt="" class="coma"></div>
+      <div class="letters-container"><img src="/images/p_1_Default.png" alt="" class="letter-p1"><img src="/images/u_Default.png" alt="" class="letter-u"><img src="/images/l_Default.png" alt="" class="letter-l"><img src="/images/p_2_Default.png" alt="" class="letter-p2"><img src="/images/e_Default.png" alt="" class="letter-e"><img src="/images/_Default.png" alt="" class="coma"></div>
     </a>
   </div>
   <div class="div-tile merci">
